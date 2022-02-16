cb86f2bf7c52bf9c95cae43a50d6d37fa4b82c6a
Luis Lopez Avalos <99110775+LuisLopezAvalos@users.noreply.github.com>
2022-02-16 11:06:46 -0600

    Agregar katas 1-10

---
+++ KATA 1.py
@@ -0,0 +1,9 @@
+## La fecha del dia de hoy
+from datetime import date
+date.today()
+print("Today's date is: "+str(date.today()))
+
+## Construir un convertidor de unidades
+parsec = 11
+lightyears = parsec*3.26156#(años luz)
+print(str(parsec) + " parsec, is " + str(lightyears) + " lightyears")
